167fcde155b17b69568a4b4b944ef706cf956728
Badshah <ankitdhoni557@gmail.com>
2025-12-18 04:24:00 +0530

    Move requirements.txt to root and update environment config

--- backend/db.py
+++ backend/db.py
@@ -1,14 +1,18 @@
 import os
 from flask_mysqldb import MySQL
+from dotenv import load_dotenv
+
+load_dotenv() # This loads the variables from your .env file
 
 mysql = MySQL()
 
 def init_db(app):
-    app.config["MYSQL_HOST"] = os.getenv("MYSQLHOST")
-    app.config["MYSQL_USER"] = os.getenv("MYSQLUSER")
-    app.config["MYSQL_PASSWORD"] = os.getenv("MYSQLPASSWORD")
-    app.config["MYSQL_DB"] = os.getenv("MYSQLDATABASE")
-    app.config["MYSQL_PORT"] = int(os.getenv("MYSQLPORT", 3306))
-    app.config["MYSQL_CURSORCLASS"] = "DictCursor"
-
+    # These will be pulled from your .env file locally 
+    # and from "Environment Variables" on Render later
+    app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
+    app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+    app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+    app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
+    app.config['MYSQL_PORT'] = int(os.getenv('MYSQL_PORT', 3306))
+    
     mysql.init_app(app)
